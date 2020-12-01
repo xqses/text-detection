741e0f6874df70fde25369a083b20c5183aecd39
n3reides <dahlstedtolle@gmail.com>
2020-12-01 15:54:17 +0100

    asd

--- main.py
+++ main.py
@@ -86,11 +86,12 @@ def main():
     fname = 'IMG_1725.JPG'
     img = open_file(fname)
     prepr_img = preprocessing(img)
+    cv2.imwrite('placeholder2.jpg', prepr_img)
     sure_fg, sure_bg = morphologize(prepr_img)
     markers = get_markers(sure_fg, sure_bg)
     color_img, markers = watershed(fname, markers)
-    dilated_img = dilate(markers, color_img)
-    find_contours(dilated_img, color_img)
+    dilated_img, color_img = dilate(markers, color_img)
+    find_contours(prepr_img)
 
 
 
@@ -101,7 +102,7 @@ def open_file(name):
 
 def preprocessing(img):
     print("entered preproc")
-    ret, thresh = cv2.threshold(img, 180, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
     img_rlsa_horizontal = rlsa.rlsa(thresh, True, False, 5)
     img_rlsa_vertical = rlsa.rlsa(img_rlsa_horizontal, False, True, 5)
     return img_rlsa_vertical
@@ -109,14 +110,14 @@ def preprocessing(img):
 def morphologize(prep_img):
     print("entered morphologize")
     opening = cv2.morphologyEx(prep_img, cv2.MORPH_OPEN, np.ones((3, 3), np.int), iterations=2)
-    # cv2.imwrite('placeholder2.jpg', opening)
+    cv2.imwrite('placeholder2.jpg', opening)
 
     sure_bg = cv2.dilate(opening, None, iterations=5)
     sure_bg = sure_bg - cv2.erode(sure_bg, None)
     dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
     dist_transform = ((dist_transform - dist_transform.min()) / (dist_transform.max() - dist_transform.min()) * 255).astype(np.uint8)
-    # cv2.imwrite('place6.jpg', sure_bg)
-    # cv2.imwrite('placeholder3.jpg', dist_transform)
+    cv2.imwrite('place6.jpg', sure_bg)
+    cv2.imwrite('placeholder3.jpg', dist_transform)
     ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
     return dist_transform, sure_bg
 
@@ -125,7 +126,7 @@ def get_markers(sure_fg, sure_bg):
     labels, ncc = label(sure_fg)
     labels = labels * labels * (255 / (ncc + 1))
     labels[sure_bg == 255] = 255
-    lbl = labels.astype(np.int32)
+    labels = labels.astype(np.int32)
     return labels
 
 def watershed(file_name, markers):
@@ -145,34 +146,35 @@ def dilate(labels, img_3C):
     # threshold result
     result[result != 255] = 0
     result = cv2.dilate(result, None)
-    # ret, thresh_res = cv2.threshold(result, 0, 255, cv2.THRESH_BINARY_INV)
+    ret, thresh_res = cv2.threshold(result, 0, 255, cv2.THRESH_BINARY_INV)
+    cv2.imwrite('placeholder4.jpg', thresh_res)
     img_3C[result == 255] = (0, 0, 255)
-    return img_3C
+    cv2.imwrite('placeholder5.jpg', img_3C)
+    return result, img_3C
 
 # find contours
-def find_contours(dilated_img, img_3C):
+def find_contours(threshold_img):
     print("finding contours")
-    contours, hierarch = cv2.findContours(dilated_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarch = cv2.findContours(threshold_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cont_counter = 0
     # list_of_bubbles = []
     print("writing contours")
     for cont in contours:
-        new_cont = np.zeros(dilated_img.shape, dtype='uint8')
+        new_cont = np.zeros(threshold_img.shape, dtype='uint8')
         imgH = new_cont.shape[0]
         imgW = new_cont.shape[1]
         x, y, w, h = cv2.boundingRect(cont)
-        if not (not (w > 30) or not (h > 50)) and h < 0.75 * imgH and w < 0.75 * imgW:
 
             # is_text = detect_text(thresh, cont)
             # if is_text:
-            #     cont_counter += 1
+        if not ((w < 30) or (h < 50)) and h < 0.75 * imgH and w < 0.75 * imgW:
+            cont_counter += 1
             cv2.drawContours(new_cont, [cont], -1, (0, 255, 0), 3)
             cv2.imwrite('written_cont' + str(cont_counter) + '.jpg', new_cont)
             # list_of_bubbles.append(cont)
             #     print(cont_counter)
             ## print(x,y,w,h)
-main()
-# img_3C = cv2.imread(file_name)
+        # img_3C = cv2.imread(file_name)
 
 #            for bubble in list_of_bubbles:
 #                cv2.drawContours(img_3C, [bubble], -1, (255,255,255), thickness = -1)
@@ -182,3 +184,4 @@ main()
 # print("total bubbles predicted by human values: " + str(count_human_true))
 # print("total bubbles predicted by decision tree: " + str(count_tree_true))
 
+main()
